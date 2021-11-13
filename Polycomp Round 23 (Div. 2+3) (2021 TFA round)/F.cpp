f7b95de9cf4f47da039e67836fe352c8ac235f7a
woolysheep1208 <87895140+woolysheep1208@users.noreply.github.com>
2021-11-13 17:40:02 +0800

    Create F.cpp

---
+++ Polycomp Round 23 (Div. 2+3) (2021 TFA round)/F.cpp
@@ -0,0 +1,53 @@
+// Rejected on Test 6
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll int, ll int>
+#define vi vector<int>
+#define vl vector<ll int>
+#define ff first
+#define ss second
+#define pb push_back
+#define pres(n,k) fixed << setprecision(k) << n
+#define debug(n) "DEBUGGING:" << n
+#define upzero(n) if(n<0) n=0
+#pragma GCC optimize("Ofast")
+ 
+// Global Variables
+ 
+ 
+void solve() {
+	// Implementation
+    int a;
+    cin >> a;
+    if(a==1) 
+		cout << "KOMPASU" << endl;
+    else if(a==2) 
+		cout << "TYPHOON" << endl;
+    else if(a==3) 
+		cout << "HECTOPASCAL" << endl;
+    else if(a==4)
+		cout << "NO.8_NORTHEAST_GALE_OR_STORM_SIGNAL" << endl;
+    else if(a==5) 
+		cout << "34" << endl;
+    else if(a==6)
+		cout << "FFFTFFTT" << endl;
+}
+	
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	srand(time(NULL));
+    solve();
+}
+ 
+/*
+ 
+ 
+*/
