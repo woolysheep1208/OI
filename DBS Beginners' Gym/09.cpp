88b4fd28af7538acbae2cf2034039c5618906373
woolysheep1208 <87895140+woolysheep1208@users.noreply.github.com>
2021-11-18 19:07:39 +0800

    Create 09.cpp

---
+++ DBS Beginners' Gym/09.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll int, ll int>
+#define vi vector<int>
+#define vl vector<ll int>
+#define ff first
+#define ss second
+#define pb push_back
+#define pres(n,k) fixed << setprecision(k) << n
+#define debug(n) "DEBUGGING:" << n
+#define upzero(n) if(n<0) n=0
+#pragma GCC optimize("Ofast")
+ 
+// Global Variables
+ 
+ 
+void solve() {
+	// Implementation
+	int a, b, c;
+	cin >> a >> b >> c;
+	int n=1;
+	while(a%n==0 || b%n==0 || c%n==0) {
+		n++;
+	}
+	cout << n << endl;
+}
+	
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	srand(time(NULL));
+    solve();
+}
+ 
+/*
+ 
+ 
+*/
