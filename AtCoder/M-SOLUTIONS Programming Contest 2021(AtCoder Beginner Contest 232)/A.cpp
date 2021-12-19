a42f1edcacff5b2cb82a673bdb7b549707df26e5
woolysheep1208 <87895140+woolysheep1208@users.noreply.github.com>
2021-12-19 21:11:40 +0800

    Create A.cpp

---
+++ AtCoder/M-SOLUTIONS Programming Contest 2021(AtCoder Beginner Contest 232)/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll int, ll int>
+#define vt vector
+#define ff first
+#define ss second
+#define pb push_back
+#define dbg fprintf(stderr, "%s %d: ", __FUNCTION__, __LINE__)
+#pragma GCC optimize("Ofast")
+
+// Global Variables
+
+
+void solve(int testcase) {
+    // Implementation
+	int a, b;
+	char fai;
+	cin >> a >> fai >> b;
+	cout << a*b << endl;
+}
+    
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    srand(time(NULL));
+    int t=1;
+//   cin >> t;
+    for (int i=1; i<=t; i++) 
+        solve(i);
+}
