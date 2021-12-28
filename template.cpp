466b4c40e2e9a80d54e599600d642be0304965a3
woolysheep1208 <87895140+woolysheep1208@users.noreply.github.com>
2021-12-28 21:58:29 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -15,8 +15,32 @@ typedef pair<ll, ll> pll;
 #define pb push_back
 #define all(x) x.begin(), x.end()
 
-#define dbgfl fprintf(stderr, "%s %d\n", __FUNCTION__, __LINE__)
-#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
 
 // Global Variables
 
@@ -25,7 +49,6 @@ void solve() {
     // Implementation
 
 }
-    
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
