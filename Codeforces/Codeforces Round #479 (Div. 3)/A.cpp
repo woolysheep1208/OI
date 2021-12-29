3d5eabb3cbe50cee58e34f5c41c020fd000ed005
woolysheep1208 <87895140+woolysheep1208@users.noreply.github.com>
2021-12-29 15:32:53 +0800

    Create A.cpp

---
+++ Codeforces/Codeforces Round #479 (Div. 3)/A.cpp
@@ -0,0 +1,44 @@
+// Virtual
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast")
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+ 
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+#define vt vector
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+ 
+#define dbgfl fprintf(stderr, "%s %d\n", __FUNCTION__, __LINE__)
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
+ 
+// Global Variables
+ 
+ 
+void solve() {
+    // Implementation
+    ll n; int k;
+    cin >> n >> k;
+    while(k--) {
+        if(n%10==0) n/=10;
+        else n--;
+    }
+    cout << n << endl;
+}
+    
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    srand(time(NULL));
+    int t=1;
+//    cin >> t;
+	while(t--) solve();
+}
